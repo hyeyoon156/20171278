23b0785f63f8699aa98a6984fd0848aebcf1b72f
user <user@532-15>
2018-10-25 13:40:50 +0900

    PP09.Inheritance

---
+++ PP09.Inheritance/PP06.Image_Sprite/Game.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <SDL.h>
+#include "TextureManager.h"
+
+class Game
+{
+	//SDL_Texture* m_pTexture; // the new SDL_Texture variable
+	//SDL_Rect m_sourceRectangle; // 원본 사각형 
+	//SDL_Rect m_destinationRectangle; // 대상 사각형
+	//SDL_Rect m_destinationRectangle2; // 대상 사각형
+	int m_currentFrame;
+	TextureManager m_textureManager;
+
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+	//SDL_Texture* m_pTexture; //
+	//SDL_Rect m_sourceRectangle; //
+	//SDL_Rect m_destinationRectangle; //
+};
+
