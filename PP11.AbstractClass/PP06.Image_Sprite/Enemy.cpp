6dc168068e208d94da06b9d1ebead385f1aaa6a0
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-11-02 12:11:08 +0900

    pp11

---
+++ PP11.AbstractClass/PP06.Image_Sprite/Enemy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Enemy.h"
+
+//void Enemy::update()
+//{
+//	GameObject::update();
+//	m_y += 1;
+//	//m_x += 1;
+//	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+//}
+Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams)
+{
+}
+void Enemy::draw()
+{
+	SDLGameObject::draw(); // we now use SDLGameObject
+}
+void Enemy::update()
+{
+	m_y += 1;
+	m_x += 1;
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+}
+void Enemy::clean()
+{
+}
